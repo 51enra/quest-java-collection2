75d6182d49feabdcaa520b191af189e751f95614
Arne Chrestin <arne.chrestin@telekom.de>
2019-11-28 17:30:54 +0100

    Quest solution version 1

--- Hero.java
+++ Hero.java
@@ -1,4 +1,4 @@
-public class Hero {
+public class Hero implements Comparable<Hero> {
 
     // attributs
     private String name;
@@ -27,4 +27,14 @@ public class Hero {
     public void setAge(int age) {
         this.age = age;
     }
+
+    // New Comparability
+    
+	@Override
+	public int compareTo(Hero hero) {
+		
+		return this.getName().compareTo(hero.getName());
+	}
+    
+    
 }
